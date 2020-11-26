4c7b3648a77a1d64d16f289c87ecff5aa7f70fe2
David Okwacha <osweredavid@gmail.com>
2020-11-26 09:50:47 +0300

    Made changes to the landing page

---
+++ RealEstate-Angular-Frontend-grp7/src/app/services/review.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { from, Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  url = '';
+  constructor(private http:HttpClient) { }
+
+get user(){
+
+  return this.http.get(this.url);
+
+}
+
+// getProduct():Observable<any>{
+//   return this.http.get("https://hood9000.herokuapp.com/api/products/all/")
+// }
+
+}
